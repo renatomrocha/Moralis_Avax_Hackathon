89c9683fed9d8b9dfbea87902ff92739a536d462
Renato Rocha <renatomrocha93@gmail.com>
2021-12-23 20:04:20 +0000

    Changed toke synchronizer

--- backend/cloud/tokenPriceSynchronizer.js
+++ backend/cloud/tokenPriceSynchronizer.js
@@ -1,28 +1,50 @@
-import {getTokenPrice, getTokenPriceHistory} from "../../avada_client/src/services/tokenService";
 import Moralis from "moralis";
+import {getBlockFromDate} from "../../avada_client/src/services/miscService";
+
+const logger = Moralis.Cloud.getLogger();
+
+const getTokenPrice = async (address , chain, to_date) => {
+
+    const options  = {
+        address: address,
+        chain: chain?chain:"avalanche"
+    };
+
+    if (to_date) {
+        const blockInfo = await getBlockFromDate(to_date);
+        Object.assign(options,{to_block: blockInfo.block});
+    }
+
+    const tokenPrice = await Moralis.Web3API.token.getTokenPrice(options);
+    console.log(`Price for token with address ${address} is ${tokenPrice}`);
+    return tokenPrice;
+}
+
+
+
+const getTokenPriceHistory = async (address, dateInterval) => {
+    const priceHistory = [];
+    for (let date of dateInterval) {
+
+        try {
+            const price = await getTokenPrice(address, "avalanche", date);
+            price["date"] = date;
+            price["address"] = address;
+            priceHistory.push(price);
+        } catch (e) {
+            logger.info("Unable to get price for date: ", date);
+        }
+
+    }
+    logger.info("Finished fetching prices");
+    logger.info(`Successfully fetched ${priceHistory.length} of ${dateInterval.length}`)
+    return priceHistory;
+}
+
+
 
 Moralis.Cloud.define("welcomeFunction", async () => {
 
-    const logger = Moralis.Cloud.getLogger();
-
-    const getTokenPriceHistory = async (address, dateInterval) => {
-        const priceHistory = [];
-        for (let date of dateInterval) {
-
-            try {
-                const price = await getTokenPrice(address, "avalanche", date);
-                price["date"] = date;
-                price["address"] = address;
-                priceHistory.push(price);
-            } catch (e) {
-                logger.info("Unable to get price for date: ", date);
-            }
-
-        }
-        logger.info("Finished fetching prices");
-        logger.info(`Successfully fetched ${priceHistory.length} of ${dateInterval.length}`)
-        return priceHistory;
-    }
 
 
     const startDate = new Date;
