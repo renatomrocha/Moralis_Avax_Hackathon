f452b1a98b94dd472d42f0ae73573a32fb2447d7
Guruprasad Kamath <guru241987@gmail.com>
2022-01-08 16:20:27 +0100

    Fetch Liquidity History Job

---
+++ backend/local/FetchPairLiquidity.py
@@ -0,0 +1,88 @@
+#!/home/ubuntu/backend/venv/bin/python
+import sys, os
+import requests
+import json
+import pymongo
+from datetime import datetime
+from helper import TraderJoeSyncEventABI
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = pymongo.MongoClient(os.getenv("PROD_REMOTE_CONNECTION_STRING"))["parse"]
+
+
+def get_pair_liquidity(address, from_time, to_time):
+
+    url = "https://deep-index.moralis.io/api/v2/" + address + "/events"
+
+    params = {
+        "chain": "avalanche",
+        "from_date": from_time,
+        "to_date": to_time,
+        "topic": "Sync(uint112,uint112)",
+        "limit": 1,
+    }
+
+    headers = {
+        "accept": "application/json",
+        "x-api-key": os.getenv("API_KEY"),
+        "Content-Type": "application/json",
+    }
+
+    response = requests.request(
+        "POST", url, headers=headers, params=params, data=TraderJoeSyncEventABI
+    )
+
+    result = response.json()["result"]
+    if len(result) == 1:
+        reserve0 = int(result[0]["data"]["reserve0"])
+        reserve1 = int(result[0]["data"]["reserve1"])
+    else:
+        reserve0 = -999
+        reserve1 = -999
+
+    return reserve0, reserve1
+
+
+if __name__ == "__main__":
+
+    control_class = pymongo.MongoClient(os.getenv("PROD_REMOTE_CONNECTION_STRING"))[
+        "parse"
+    ]["DexTokenPairControl"]
+    target_class = pymongo.MongoClient(os.getenv("PROD_REMOTE_CONNECTION_STRING"))[
+        "parse"
+    ]["DexTokenPairLiquidity"]
+    pairs = control_class.find()
+
+    _interval = 24 * 60 * 60
+
+    for pair in pairs:
+
+        curr_time = pair["timeStamp"] + _interval
+        max_time = int(datetime.now().timestamp())
+
+        while curr_time + _interval < max_time:
+            pair.pop("_id")
+
+            reserve0, reserve1 = get_pair_liquidity(
+                pair["pairAddress"], curr_time, curr_time + _interval
+            )
+
+            if reserve0 != -999:
+                pair["reserve0"] = reserve0 / (10 ** pair["decimal0"])
+                pair["reserve1"] = reserve1 / (10 ** pair["decimal1"])
+
+            pair["timeStamp"] = curr_time
+
+            control_class.replace_one(
+                {"pairAddress": pair["pairAddress"]}, pair, upsert=True
+            )
+
+            pair["createdAt"] = datetime.now()
+            pair["updatedAt"] = datetime.now()
+
+            target_class.insert_one(pair)
+
+            curr_time += _interval
