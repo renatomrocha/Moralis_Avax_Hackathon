b5712c60bd21fdd4a252b41a298baa4b00a55e92
Guruprasad Kamath <guru241987@gmail.com>
2022-01-09 09:39:19 +0100

    Calculate Correlations

---
+++ backend/local/RunScripts.sh
@@ -0,0 +1,23 @@
+source venv/bin/activate
+
+echo "calculating daily price summary..."
+python local/GetSummary.py Token1Day
+echo "daily price summary done!"
+
+echo "calculating 4 hourly price summary..."
+python local/GetSummary.py Token4Hour
+echo "4 hourly price summary done!"
+
+echo "calculating hourly price summary..."
+python local/GetSummary.py Token1Hour
+echo "hourly price summary done!"
+
+echo "calculating 30 day token price correlations..."
+python local/GetCorrelation.py
+echo "correlations done!"
+
+echo "fetching token pair liquidity..."
+python local/FetchPairLiquidity.py
+echo "token pair liquidity updated!"
+
+deactivate
