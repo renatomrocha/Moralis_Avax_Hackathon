38927c5e8559401145e7aed5566c040400388829
Guruprasad Kamath <guru241987@gmail.com>
2022-01-14 18:52:34 +0100

    Get Historical Market Cap

--- backend/local/HistoricalMarketCap.py
+++ backend/local/HistoricalMarketCap.py
@@ -4,55 +4,62 @@ from dotenv import load_dotenv
 from datetime import datetime
 import pymongo
 import logging
+import json
+from web3 import Web3
 
-from helper import ERC20TotalSupply
+from helper import ERC20TotalSupply, block_number_getter, TokenDetails
 
 load_dotenv()
 
-import requests
-import json
-
-_from = pymongo.MongoClient(os.getenv('PROD'))['parse']['Token1Day']
-objects = _from.find({})
-
-for obj in objects:
-
-    _decimal_class = pymongo.MongoClient(os.getenv('PROD'))['parse']['TokenDetails']
-    _decimal = _decimal_class.find_one({'address': obj['address']})['decimals']
-
-    url = "https://deep-index.moralis.io/api/v2/" + obj['address'] + "/function"
-
-    params = {
-        'chain': 'avalanche',
-        'function_name': 'totalSupply',
-        'to_date': obj['timeStamp'],
-    }
+w3 = Web3(Web3.HTTPProvider(os.getenv('SPEEDY_NODE')))
 
 
-    headers = {
-    'accept': 'application/json',
-    "x-api-key": os.getenv("API_KEY"),
-    'Content-Type': 'application/json'
-    }
+_from = pymongo.MongoClient(os.getenv('KONS'))['parse']['Token15Min']
 
-    
-    parse_successful = False
-    count = 0
-    max_count = 5
-    while (not parse_successful) and count < max_count:
-        count += 1
+
+start_time = 1629417600
+interval = 24 * 60 * 60
+end_time = 1642178693
+
+curr_time = start_time
+
+while curr_time + interval < end_time:
+
+    block = block_number_getter('avalanche', curr_time)
+
+    for token in TokenDetails:
+
+        objects = _from.find({'timeStamp': {'$gte': curr_time, '$lt': curr_time + interval}, 'symbol': token['symbol']})
+
+        contract_function = w3.eth.contract(address=Web3.toChecksumAddress(token['address']), abi=ERC20TotalSupply)
+
         try:
-            response = requests.request("POST", url, headers=headers, params=params, data=ERC20TotalSupply)
-            obj['totalSupply'] = int(response.json()) / (10 ** _decimal)
-            parse_successful = True
+            _total_supply = contract_function.functions.totalSupply().call(block_identifier=block)
         except:
-            obj['totalSupply'] = 0
-            if count == 5:
-                print("Max tries exceeded for {} at timestamp {}".format(obj['symbol'],obj['timestamp']))
+            _total_supply = None
 
-    obj['marketCap'] = obj['totalSupply'] * obj['averagePrice']
+        for obj in objects:
+            obj['totalSupply'] = _total_supply / 10 ** token['decimals']
+            obj['marketCap'] = _total_supply * obj['price'] 
+            _from.replace_one({'_id': obj['_id']}, obj)
 
-    # _from.replace_one({'_id': obj['_id']}, obj)
+    print('Updated timestamp {}'.format(curr_time))
 
-    print(obj['symbol'], obj['timeStamp'], obj['totalSupply'], obj['marketCap'])
+
+    curr_time = curr_time + interval
+
+# for obj in objects:
+
+#     _decimal_class = pymongo.MongoClient(os.getenv('PROD'))['parse']['TokenDetails']
+#     _decimal = _decimal_class.find_one({'address': obj['address']})['decimals']
+
+#     block = block_number_getter('avalanche', obj['timeStamp'])
+
+#     contract_function = w3.eth.contract(address=Web3.toChecksumAddress(obj['address']), abi=ERC20TotalSupply)
+
+#     obj['totalSupply'] = contract_function.functions.totalSupply().call(block_identifier=block) / 
+
+#     obj['marketCap'] = obj['averagePrice'] * obj['totalSupply']
+
+#     _from.replace_one({'_id': obj['_id']}, obj)
 
--- backend/local/helper.py
+++ backend/local/helper.py
@@ -1,41 +1,77 @@
+import os
+import requests
 import pytz
 import json
 from datetime import datetime
 
+# TokenDetails = [
+#     {"symbol": "WETH.e", "address": "0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab"},
+#     {"symbol": "USDT.e", "address": "0xc7198437980c041c805a1edcba50c1ce5db95118"},
+#     {"symbol": "WAVAX.e", "address": "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7"},
+#     {"symbol": "UNI.e", "address": "0x8ebaf22b6f053dffeaf46f4dd9efa95d89ba8580"},
+#     {"symbol": "LINK.e", "address": "0x5947bb275c521040051d82396192181b413227a3"},
+#     {"symbol": "AAVE.e", "address": "0x63a72806098bd3d9520cc43356dd78afe5d386d9"},
+#     {"symbol": "1INCH.e", "address": "0xd501281565bf7789224523144fe5d98e8b28f267"},
+#     {"symbol": "SUSHI.e", "address": "0x37b608519f91f70f2eeb0e5ed9af4061722e4f76"},
+#     {"symbol": "FRAX", "address": "0xd24c2ad096400b6fbcd2ad8b24e7acbc21a1da64"},
+#     {"symbol": "ANY", "address": "0xb44a9b6905af7c801311e8f4e76932ee959c663c"},
+#     {"symbol": "FXS", "address": "0x214db107654ff987ad859f34125307783fc8e387"},
+#     {"symbol": "JOE", "address": "0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd"},
+#     {"symbol": "WALBT", "address": "0x9e037de681cafa6e661e6108ed9c2bd1aa567ecd"},
+#     {"symbol": "SWAP.e", "address": "0xc7b5d72c836e718cda8888eaf03707faef675079"},
+#     {"symbol": "BIFI", "address": "0xd6070ae98b8069de6b494332d1a1a81b6179d960"},
+#     {"symbol": "PNG", "address": "0x60781c2586d68229fde47564546784ab3faca982"},
+#     {"symbol": "QI", "address": "0x8729438eb15e2c8b576fcc6aecda6a148776c0f5"},
+#     {"symbol": "DYP", "address": "0x961c8c0b1aad0c0b10a51fef6a867e3091bcef17"},
+#     {"symbol": "SNOB", "address": "0xc38f41a296a4493ff429f1238e030924a1542e50"},
+#     {"symbol": "XAVA", "address": "0xd1c3f94de7e5b45fa4edbba472491a9f4b166fc4"},
+#     {"symbol": "YAK", "address": "0x59414b3089ce2af0010e7523dea7e2b35d776ec7"},
+#     {"symbol": "USDC.e", "address": "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664"},
+#     {"symbol": "DAI.e", "address": "0xd586e7f844cea2f87f50152665bcbc2c279d8d70"},
+#     {"symbol": "CRA", "address": "0xa32608e873f9ddef944b24798db69d80bbb4d1ed"},
+#     {"symbol": "OOE", "address": "0x0ebd9537a25f56713e34c45b38f421a1e7191469"},
+#     {"symbol": "MIM", "address": "0x130966628846bfd36ff31a822705796e8cb8c18d"},
+#     {"symbol": "HCT", "address": "0x45c13620b55c35a5f539d26e88247011eb10fdbd"},
+#     {"symbol": "CLY", "address": "0xec3492a2508ddf4fdc0cd76f31f340b30d1793e6"},
+#     {"symbol": "SMRTr", "address": "0x6d923f688c7ff287dc3a5943caeefc994f97b290"},
+#     {"symbol": "TIME", "address": "0xb54f16fb19478766a268f172c9480f8da1a7c9c3"},
+#     {"symbol": "xJOE", "address": "0x57319d41f71e81f3c65f2a47ca4e001ebafd4f33"},
+#     {"symbol": "WBTC.e", "address": "0x50b7545627a5162f82a992c33b87adc75187b218"},
+# ]
+
 TokenDetails = [
-    {"symbol": "WETH.e", "address": "0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab"},
-    {"symbol": "USDT.e", "address": "0xc7198437980c041c805a1edcba50c1ce5db95118"},
-    {"symbol": "WAVAX.e", "address": "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7"},
-    {"symbol": "UNI.e", "address": "0x8ebaf22b6f053dffeaf46f4dd9efa95d89ba8580"},
-    {"symbol": "LINK.e", "address": "0x5947bb275c521040051d82396192181b413227a3"},
-    {"symbol": "AAVE.e", "address": "0x63a72806098bd3d9520cc43356dd78afe5d386d9"},
-    {"symbol": "1INCH.e", "address": "0xd501281565bf7789224523144fe5d98e8b28f267"},
-    {"symbol": "SUSHI.e", "address": "0x37b608519f91f70f2eeb0e5ed9af4061722e4f76"},
-    {"symbol": "FRAX", "address": "0xd24c2ad096400b6fbcd2ad8b24e7acbc21a1da64"},
-    {"symbol": "ANY", "address": "0xb44a9b6905af7c801311e8f4e76932ee959c663c"},
-    {"symbol": "FXS", "address": "0x214db107654ff987ad859f34125307783fc8e387"},
-    {"symbol": "JOE", "address": "0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd"},
-    {"symbol": "WALBT", "address": "0x9e037de681cafa6e661e6108ed9c2bd1aa567ecd"},
-    {"symbol": "SWAP.e", "address": "0xc7b5d72c836e718cda8888eaf03707faef675079"},
-    {"symbol": "BIFI", "address": "0xd6070ae98b8069de6b494332d1a1a81b6179d960"},
-    {"symbol": "PNG", "address": "0x60781c2586d68229fde47564546784ab3faca982"},
-    {"symbol": "QI", "address": "0x8729438eb15e2c8b576fcc6aecda6a148776c0f5"},
-    {"symbol": "DYP", "address": "0x961c8c0b1aad0c0b10a51fef6a867e3091bcef17"},
-    {"symbol": "SNOB", "address": "0xc38f41a296a4493ff429f1238e030924a1542e50"},
-    {"symbol": "XAVA", "address": "0xd1c3f94de7e5b45fa4edbba472491a9f4b166fc4"},
-    {"symbol": "YAK", "address": "0x59414b3089ce2af0010e7523dea7e2b35d776ec7"},
-    {"symbol": "USDC.e", "address": "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664"},
-    {"symbol": "DAI.e", "address": "0xd586e7f844cea2f87f50152665bcbc2c279d8d70"},
-    {"symbol": "CRA", "address": "0xa32608e873f9ddef944b24798db69d80bbb4d1ed"},
-    {"symbol": "OOE", "address": "0x0ebd9537a25f56713e34c45b38f421a1e7191469"},
-    {"symbol": "MIM", "address": "0x130966628846bfd36ff31a822705796e8cb8c18d"},
-    {"symbol": "HCT", "address": "0x45c13620b55c35a5f539d26e88247011eb10fdbd"},
-    {"symbol": "CLY", "address": "0xec3492a2508ddf4fdc0cd76f31f340b30d1793e6"},
-    {"symbol": "SMRTr", "address": "0x6d923f688c7ff287dc3a5943caeefc994f97b290"},
-    {"symbol": "TIME", "address": "0xb54f16fb19478766a268f172c9480f8da1a7c9c3"},
-    {"symbol": "xJOE", "address": "0x57319d41f71e81f3c65f2a47ca4e001ebafd4f33"},
-    {"symbol": "WBTC.e", "address": "0x50b7545627a5162f82a992c33b87adc75187b218"},
-]
+    {'name': 'Ethereum', 'symbol': 'WETH.e', 'address': '0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab', 'decimals': 18}, 
+    {'name': 'Tether', 'symbol': 'USDT.e', 'address': '0xc7198437980c041c805a1edcba50c1ce5db95118', 'decimals': 6}, 
+    {'name': 'Avalanche', 'symbol': 'WAVAX.e', 'address': '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7', 'decimals': 18}, 
+    {'name': 'Uniswap', 'symbol': 'UNI.e', 'address': '0x8ebaf22b6f053dffeaf46f4dd9efa95d89ba8580', 'decimals': 18}, 
+    {'name': 'Chainlink', 'symbol': 'LINK.e', 'address': '0x5947bb275c521040051d82396192181b413227a3', 'decimals': 18}, 
+    {'name': 'Aave', 'symbol': 'AAVE.e', 'address': '0x63a72806098bd3d9520cc43356dd78afe5d386d9', 'decimals': 18}, 
+    {'name': '1INCH Token', 'symbol': '1INCH.e', 'address': '0xd501281565bf7789224523144fe5d98e8b28f267', 'decimals': 18}, 
+    {'name': 'SushiSwap', 'symbol': 'SUSHI.e', 'address': '0x37b608519f91f70f2eeb0e5ed9af4061722e4f76', 'decimals': 18}, 
+    {'name': 'Frax', 'symbol': 'FRAX', 'address': '0xd24c2ad096400b6fbcd2ad8b24e7acbc21a1da64', 'decimals': 18}, 
+    {'name': 'AnySwap', 'symbol': 'ANY', 'address': '0xb44a9b6905af7c801311e8f4e76932ee959c663c', 'decimals': 18}, 
+    {'name': 'Frax Share', 'symbol': 'FXS', 'address': '0x214db107654ff987ad859f34125307783fc8e387', 'decimals': 18}, 
+    {'name': 'JOE', 'symbol': 'JOE', 'address': '0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd', 'decimals': 18}, 
+    {'name': 'AllianceBlock', 'symbol': 'WALBT', 'address': '0x9e037de681cafa6e661e6108ed9c2bd1aa567ecd', 'decimals': 18}, 
+    {'name': 'TrustSwap', 'symbol': 'SWAP.e', 'address': '0xc7b5d72c836e718cda8888eaf03707faef675079', 'decimals': 18}, 
+    {'name': 'Beefy Finance', 'symbol': 'BIFI', 'address': '0xd6070ae98b8069de6b494332d1a1a81b6179d960', 'decimals': 18}, 
+    {'name': 'Pangolin', 'symbol': 'PNG', 'address': '0x60781c2586d68229fde47564546784ab3faca982', 'decimals': 18}, 
+    {'name': 'BENQI', 'symbol': 'QI', 'address': '0x8729438eb15e2c8b576fcc6aecda6a148776c0f5', 'decimals': 18}, 
+    {'name': 'DeFi Yield Protocol', 'symbol': 'DYP', 'address': '0x961c8c0b1aad0c0b10a51fef6a867e3091bcef17', 'decimals': 18}, 
+    {'name': 'Snowball', 'symbol': 'SNOB', 'address': '0xc38f41a296a4493ff429f1238e030924a1542e50', 'decimals': 18}, 
+    {'name': 'Avalaunch', 'symbol': 'XAVA', 'address': '0xd1c3f94de7e5b45fa4edbba472491a9f4b166fc4', 'decimals': 18}, 
+    {'name': 'Yield Yak', 'symbol': 'YAK', 'address': '0x59414b3089ce2af0010e7523dea7e2b35d776ec7', 'decimals': 18}, 
+    {'name': 'USD Coin', 'symbol': 'USDC.e', 'address': '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664', 'decimals': 6}, 
+    {'name': 'DAI', 'symbol': 'DAI.e', 'address': '0xd586e7f844cea2f87f50152665bcbc2c279d8d70', 'decimals': 18}, 
+    {'name': 'Crabada', 'symbol': 'CRA', 'address': '0xa32608e873f9ddef944b24798db69d80bbb4d1ed', 'decimals': 18}, 
+    {'name': 'OpenOcean', 'symbol': 'OOE', 'address': '0x0ebd9537a25f56713e34c45b38f421a1e7191469', 'decimals': 18}, 
+    {'name': 'Magic Internet Money', 'symbol': 'MIM', 'address': '0x130966628846bfd36ff31a822705796e8cb8c18d', 'decimals': 18}, 
+    {'name': 'HurricaneSwap Token', 'symbol': 'HCT', 'address': '0x45c13620b55c35a5f539d26e88247011eb10fdbd', 'decimals': 18}, 
+    {'name': 'Colony', 'symbol': 'CLY', 'address': '0xec3492a2508ddf4fdc0cd76f31f340b30d1793e6', 'decimals': 18}, 
+    {'name': 'SmarterCoin', 'symbol': 'SMRTr', 'address': '0x6d923f688c7ff287dc3a5943caeefc994f97b290', 'decimals': 18}, 
+    {'name': 'Time', 'symbol': 'TIME', 'address': '0xb54f16fb19478766a268f172c9480f8da1a7c9c3', 'decimals': 9}, 
+    {'name': 'JoeBar', 'symbol': 'xJOE', 'address': '0x57319d41f71e81f3c65f2a47ca4e001ebafd4f33', 'decimals': 18}, 
+    {'name': 'Wrapped BTC', 'symbol': 'WBTC.e', 'address': '0x50b7545627a5162f82a992c33b87adc75187b218', 'decimals': 8}]
 
 TraderJoePairCreatedEventABI = json.dumps(
     {
@@ -93,8 +129,7 @@ TraderJoeSyncEventABI = json.dumps(
     }
 )
 
-ERC20TotalSupply = json.dumps({
-  "abi": [
+ERC20TotalSupply = json.dumps([
     {
       "inputs": [],
       "name": "totalSupply",
@@ -108,9 +143,8 @@ ERC20TotalSupply = json.dumps({
       "stateMutability": "view",
       "type": "function"
     }
-  ],
-  "params": {}
-})
+  ]
+)
 
 
 def get_next_timestamp(summary_type, ts):
@@ -158,3 +192,24 @@ def make_dictionary(collection, primary_key_field, secondary_key_fields):
         primary_dict[item[primary_key_field]] = secondary_dict
 
     return primary_dict
+
+# get the blocknumber given the timeStamp or date
+def block_number_getter(chain, date_time):
+    request_url = "https://deep-index.moralis.io/api/v2/dateToBlock"
+    params = {"chain": chain, "date": date_time}
+    headers = {"x-api-key": os.getenv("API_KEY")}
+
+    parse_successful = False
+    while not parse_successful:
+        r = requests.get(request_url, params=params, headers=headers)
+        try:
+            result = r.json()["block"]
+            parse_successful = True
+        except:
+            logging.warning(
+                "False block response at timestamp {}. Trying again...".format(
+                    date_time
+                )
+            )
+
+    return result
