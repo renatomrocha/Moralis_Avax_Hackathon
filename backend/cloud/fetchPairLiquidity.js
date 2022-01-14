8f24d6380c3f971d262b986d0a07399129b6fd62
Guruprasad Kamath <48196632+gurukamath@users.noreply.github.com>
2022-01-14 15:00:37 +0100

    Fetch total supply for all Tokens (#14)

--- backend/cloud/fetchPairLiquidity.js
+++ backend/cloud/fetchPairLiquidity.js
@@ -1,54 +1,73 @@
-Moralis.Cloud.job("FetchPairLiquidity", async (request) => {
+Moralis.Cloud.job("FetchTokenDetails", async (request) => {
 
   Moralis.settings.setAPIRateLimit({
   anonymous:100, authenticated:100, windowMs:60000
 })
 
-// Get Liquidity
-// const logger = Moralis.Cloud.getLogger();
+  const logger = Moralis.Cloud.getLogger();
 
-const abi = Moralis.Object.extend("ABIs");
-const query1 = new Moralis.Query(abi);
-query1.equalTo('name', 'TraderJoePairGetReservesFunction');
-const result1 = (await query1.find())[0];
-const functionABI = JSON.parse(result1.get('abi'));
+  const TOKEN = Moralis.Object.extend("TokenDetails");
+  const query = new Moralis.Query(TOKEN);
+  query.select("symbol", "address", 'abi', 'decimals', 'canFetchTokenDetails');
+  const results = await query.find();
 
-const pairClass = Moralis.Object.extend("DexTokenPairDetails");
-const query2 = new Moralis.Query(pairClass);
-query2.equalTo('in_database', 1);
-const pairs = await query2.find();
+  // logger.info(`Total number of tokens: ${results.length}`)
+
+  for (i=0; i<results.length; i++) {
+    const r = results[i];
+    if (r.get('canFetchTokenDetails') === 1){
+      let price;
+      let totalSupply;
+      let marketCap;
+      try{
+        const priceOptions = {address: r.get("address"), chain: "avalanche",};
+        const priceResult = await Moralis.Web3API.token.getTokenPrice(priceOptions)
+        price = priceResult.usdPrice;
+      } catch (e) {
+        // logger.error(`${e}`);
+        // logger.info(`Failed to fetch price for token: ${r.get("symbol")}`);
+      }
+
+      try{
+        const strAbi = [{"inputs": [],"name": "totalSupply","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"}]
+        // const strAbi = JSON.parse(r.get("abi").toString().replaceAll("'",'"').replaceAll("False",'"False"').replaceAll("True",'"True"'))
+        const mcapOptions = {chain: "avalanche", address: r.get("address"), function_name: "totalSupply", abi: strAbi};
+        totalSupply = parseInt(await Moralis.Web3API.native.runContractFunction(mcapOptions));
+      } catch (e) {
+        // logger.error(`${e}`);
+        // logger.info(`Failed to fetch totalSupply for token: ${r.get("symbol")}`);
+      }
+
+      marketCap = (totalSupply / 10**r.get("decimals")) * price;
+
+      const Token15Min = Moralis.Object.extend("Token15Min");
+      const newTokenDetail = new Token15Min();
+
+      newTokenDetail.set('symbol', r.get("symbol"));
+      newTokenDetail.set('address', r.get("address"));
+      newTokenDetail.set('timeStamp', parseInt(new Date().getTime()/1000)),
+      newTokenDetail.set('price', price);
+      newTokenDetail.set('totalSupply', totalSupply);
+      newTokenDetail.set('marketCap', marketCap);
+
+      newTokenDetail.save()
+    }
+    
+
+  }
+
+});
 
 
-for (i=0; i<pairs.length; i++){
-    const pair = pairs[i];
-    // logger.info(pair.get("pair_address"))
-    const _address = pair.get("pair_address");
-    const _reserves = await Moralis.Web3API.native.runContractFunction({
-            chain: "avalanche",
-            address: _address,
-            function_name: "getReserves",
-            abi: functionABI,
-    })
+Moralis.Cloud.define("timeStampUpdater", async (request) => {
+  const TokenPrices = Moralis.Object.extend("TokenPrices");
+  const query = new Moralis.Query(TokenPrices);
+  query.select("createdAt");
+  query.limit(5000);
+  const results = await query.find();
 
-    const liquidityClass = Moralis.Object.extend("DexTokenPairLiquidity");
-    const liquidity = new liquidityClass();
-
-    liquidity.set('dexName', pair.get("dex_name"));
-    liquidity.set('pairAddress', pair.get("pair_address"));
-    liquidity.set('timeStamp', parseInt(new Date().getTime()/1000))
-    liquidity.set('tokenName0', pair.get("token0_name"));
-    liquidity.set('tokenName1', pair.get("token1_name"));
-    liquidity.set('tokenSymbol0', pair.get("token0_symbol"));
-    liquidity.set('tokenSymbol1', pair.get("token1_symbol"));
-    liquidity.set('tokenAddress0', pair.get("token0_address"));
-    liquidity.set('tokenAddress1', pair.get("token1_address"));
-    liquidity.set('tokenReserve0', _reserves["_reserve0"]);
-    liquidity.set('tokenReserve1', _reserves["_reserve1"]);
-    liquidity.set('blockTimeStamp', _reserves["_blockTimestampLast"]);
-
-    liquidity.save();
-
-}
-
-
-})
+  results.map((item) => {
+    item.set("timeStamp", item.get("createdAt").getTime());
+    item.save();
+  });
+});
