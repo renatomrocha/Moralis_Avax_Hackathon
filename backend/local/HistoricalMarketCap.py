3718b967bd4a45493454056e079369c8acaeccc3
Guruprasad Kamath <48196632+gurukamath@users.noreply.github.com>
2022-01-14 19:38:44 +0100

    Market cap history (#16)
    * History Market Cap Incomplete
    
    * Get Historical Market Cap
    
    * Get Historical Market Cap

---
+++ backend/local/HistoricalMarketCap.py
@@ -0,0 +1,70 @@
+import os, sys
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+import pymongo
+import logging
+import json
+from web3 import Web3
+
+from helper import ERC20TotalSupply, block_number_getter, TokenDetails
+
+load_dotenv()
+
+w3 = Web3(Web3.HTTPProvider(os.getenv('SPEEDY_NODE')))
+
+
+_from = pymongo.MongoClient(os.getenv('KONS'))['parse']['Token15Min']
+
+
+start_time = 1629417600
+interval = 24 * 60 * 60
+end_time = 1642178693
+
+curr_time = start_time
+
+while curr_time + interval < end_time:
+
+    block = block_number_getter('avalanche', curr_time)
+
+    for token in TokenDetails:
+
+        objects = _from.find({'timeStamp': {'$gte': curr_time, '$lt': curr_time + interval}, 'symbol': token['symbol']})
+
+        contract_function = w3.eth.contract(address=Web3.toChecksumAddress(token['address']), abi=ERC20TotalSupply)
+
+        try:
+            _total_supply = contract_function.functions.totalSupply().call(block_identifier=block)
+        except:
+            _total_supply = -999
+
+        for obj in objects:
+            if _total_supply != -999:
+                obj['totalSupply'] = _total_supply / 10 ** token['decimals']
+                obj['marketCap'] = _total_supply * obj['price']      
+            else:
+                obj['totalSupply'] = -999
+                obj['marketCap'] = -999
+
+            _from.replace_one({'_id': obj['_id']}, obj)
+
+    print('Updated timestamp {}'.format(curr_time))
+
+
+    curr_time = curr_time + interval
+
+# for obj in objects:
+
+#     _decimal_class = pymongo.MongoClient(os.getenv('PROD'))['parse']['TokenDetails']
+#     _decimal = _decimal_class.find_one({'address': obj['address']})['decimals']
+
+#     block = block_number_getter('avalanche', obj['timeStamp'])
+
+#     contract_function = w3.eth.contract(address=Web3.toChecksumAddress(obj['address']), abi=ERC20TotalSupply)
+
+#     obj['totalSupply'] = contract_function.functions.totalSupply().call(block_identifier=block) / 
+
+#     obj['marketCap'] = obj['averagePrice'] * obj['totalSupply']
+
+#     _from.replace_one({'_id': obj['_id']}, obj)
+
